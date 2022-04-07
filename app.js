23bdd6f02b0c846fb261dbcb88db65b898b06bb8
Pak Yulia <kristalo4ka@gmail.com>
2022-04-07 15:11:43 +0300

    adding buttons - Start, Clear Canvas, Finish

--- app.js
+++ app.js
@@ -1,10 +1,50 @@
-// Select color input
-// Select size input
 
-// When size is submitted by the user, call makeGrid()
+const inputHeight = document.getElementById('inputHeight');
+const inputWidth = document.getElementById('inputWidth');
+const sizePicker = document.getElementById('sizePicker');
+const canvas = document.getElementById('pixelCanvas');
+const colorPicker = document.getElementById('colorPicker');
+const start = document.getElementById('submit');
+const clear = document.getElementById('clearButton');
+const finish = document.getElementById('finishButton');
+const inputNumH = document.getElementById('inputHeight');
+const inputNumW = document.getElementById('inputWidth');
 
-function makeGrid() {
+start.addEventListener('click',(event) => {
+    event.preventDefault();
+    makeGrid();
+    start.disabled = true;
+    clearButton.disabled = false;
+});
+clear.addEventListener('click',(event) => {
+    event.preventDefault();
+    clearGrid();
+    makeGrid();
+});
+finish.addEventListener('click', (event) => {
+    event.preventDefault();
+    clearGrid();
+    start.disabled = false;
+    inputNumH.value = 1;
+    inputNumW.value = 1;
+});
 
-// Your code goes here!
-
+function makeGrid () {
+    for(let h=0; h<inputHeight.value; h++) {
+        const row = canvas.insertRow(h);
+        for (let w=0; w<inputWidth.value; w++) {
+            const cells = row.insertCell(w);
+            cells.addEventListener('click', () => {
+                cells.setAttribute('style',`background-color: ${colorPicker.value}`)
+            })
+        }
+    }
 }
+
+function clearGrid() {
+    while(canvas.firstChild){
+        canvas.removeChild(canvas.firstChild)
+    }
+}
+
+
